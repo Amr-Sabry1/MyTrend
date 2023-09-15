103a9e43be4d981cd17f00927066d622bc5d29e5
AmrSabryA <95857185+AmrSabryA@users.noreply.github.com>
2023-09-15 11:16:43 +0300

    Coding

--- component/LayOut.jsx
+++ component/LayOut.jsx
@@ -1,13 +1,11 @@
 import React from "react";
 import Navbar from "./NavBar";
-import Footer from "@/component/Footer";
 
 export default function LayOut(props) {
   return (
     <>
       <Navbar />
       {props.children}
-      <Footer />
     </>
   );
 }
